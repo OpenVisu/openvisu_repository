baa212b68a7e063a45154bbb407d840af0b1935d
Robin Jespersen <jespersen@unitedpartners.de>
2022-06-15 19:05:24 +0200

    added tests

---
+++ test/repositories/dashboard_test.dart
@@ -0,0 +1,66 @@
+// Copyright (C) 2022 Robin Jespersen
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import 'package:openvisu_repository/openvisu_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('AuthenticationRepository', () {
+    late AuthenticationRepository authenticationRepository;
+    late CredentialsRepository credentialsRepository;
+    late DashboardRepository dashboardRepository;
+
+    const Credentials credentialsAdmin = Credentials(
+      username: 'admin',
+      password: 'password',
+      endpoint: 'http://localhost/',
+    );
+
+    final Pk<Dashboard> id1 = Pk<Dashboard>(1);
+
+    setUp(() async {
+      credentialsRepository = CredentialsRepository();
+      authenticationRepository = AuthenticationRepository(
+        credentialsRepository: credentialsRepository,
+        httpTimeOut: const Duration(seconds: 10),
+      );
+      dashboardRepository = DashboardRepository(
+        authenticationRepository: authenticationRepository,
+      );
+
+      await authenticationRepository.authenticate(
+        credentials: credentialsAdmin,
+        saveLogin: false,
+      );
+    });
+
+    tearDown(() async {
+      await authenticationRepository.doLogout();
+    });
+
+    test('test authentication.authenticate().', () async {
+      final List<Dashboard> list = await dashboardRepository.all(null);
+      expect(list.length, 1);
+      expect(list.first.name, 'Dashboard 1');
+      expect(list.first.id, id1);
+    });
+
+    test('test authentication.authenticate().', () async {
+      final Dashboard dashboard1 = await dashboardRepository.get(id1);
+      expect(dashboard1.name, 'Dashboard 1');
+      expect(dashboard1.id, id1);
+    });
+  });
+}
