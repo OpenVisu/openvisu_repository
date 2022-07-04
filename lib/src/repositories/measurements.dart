9013b1812936f66d343c4fbfe6937ce8dc221346
Robin Jespersen <jespersen@unitedpartners.de>
2022-07-04 11:26:44 +0200

    added helper method

--- lib/src/repositories/measurements.dart
+++ lib/src/repositories/measurements.dart
@@ -138,4 +138,25 @@ class MeasurementsRepository {
       measurements.last,
     ); // TODO test if last is always the newest
   }
+
+  // must match backend/modules/dashboard/models/TimeSerial->getEveryInSeconds()
+  Duration getTimeFrameResolution(final Duration $timeframe) {
+    if ($timeframe < const Duration(minutes: 1)) {
+      return const Duration(seconds: 1);
+    } else if ($timeframe < const Duration(minutes: 15)) {
+      return const Duration(seconds: 10);
+    } else if ($timeframe < const Duration(hours: 1)) {
+      return const Duration(minutes: 1);
+    } else if ($timeframe < const Duration(hours: 8)) {
+      return const Duration(minutes: 10);
+    } else if ($timeframe < const Duration(days: 3)) {
+      return const Duration(hours: 1);
+    } else if ($timeframe < const Duration(days: 7)) {
+      return const Duration(hours: 2);
+    } else if ($timeframe < const Duration(days: 90)) {
+      return const Duration(days: 1);
+    } else {
+      return const Duration(days: 7);
+    }
+  }
 }
